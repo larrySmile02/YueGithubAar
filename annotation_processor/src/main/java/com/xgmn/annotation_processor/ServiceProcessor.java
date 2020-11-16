52bec5c736f68fa2bd8b8a52c79d082992168e89
李越 <mrli126@126.com>
2020-11-16 20:46:48 +0800

    Feat: annotation processor

---
+++ annotation_processor/src/main/java/com/xgmn/annotation_processor/ServiceProcessor.java
@@ -0,0 +1,53 @@
+package com.xgmn.annotation_processor;
+
+import com.google.auto.service.AutoService;
+import com.xgmn.annotation_service.MicroService;
+
+import java.util.Collections;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.TypeElement;
+
+@AutoService(Processor.class)
+public class ServiceProcessor extends AbstractProcessor {
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnvironment) {
+        super.init(processingEnvironment);
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+       return Collections.singleton(MicroService.class.getCanonicalName());
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+
+        for(Element element:roundEnvironment.getRootElements()){
+            System.out.print("ServiceProcessor_elementName="+element.getSimpleName());
+            for(Element enclosedElement : element.getEnclosedElements()){
+                if(enclosedElement.getKind()== ElementKind.CLASS){
+                    MicroService service = enclosedElement.getAnnotation(MicroService.class);
+                    if(service !=null){
+                        System.out.print("ServiceProcessor_service exist!");
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
